762a4a1ea25a9e5d3c309cb3469fad9e51f2e8e6
Daniel Grunkin <dawg@trustpilot.com>
2018-04-22 14:27:22 +0200

    add tom

---
+++ rooms/door.gd
@@ -0,0 +1,8 @@
+extends StaticBody2D
+
+export(NodePath) var spawn_point 
+export(PackedScene) var to_room
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
